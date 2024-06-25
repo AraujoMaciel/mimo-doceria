45ed52008734b2145235c6bdd400f8bddf734b98
Maciel <macielsemr@gmail.com>
2024-06-25 11:57:50 -0300

    feat: criando menu

--- src/componentes/header/header.tsx
+++ src/componentes/header/header.tsx
@@ -3,11 +3,11 @@ import { IoIosMenu } from "react-icons/io";
 
 export default function Header(){
     return(
-        <div>
+        <div className="mb-4">
             <div className="bg-rosa1">
                 <div className="flex items-center justify-center ">
                     <Image
-                        className="bg-slate-50 rounded-full m-3"
+                        className="bg-white rounded-full m-3"
                         src="/Logo-mimos.png"
                         width={80}
                         height={80}
@@ -16,16 +16,16 @@ export default function Header(){
                 </div>
                 <div className="bg-rosa2 flex justify-center py-2 shadow-lg">
                     <IoIosMenu 
-                        className="text-slate-100 text-2xl"
+                        className="text-slate-100 text-2xl hover:cursor-pointer"
                     />
                 </div>
                 
             </div>
-            <div>
-                <ul>
-                    <li>Home</li>
-                    <li>Menu</li>
-                    <li>Saiba Mais</li>
+            <div className="bg-rosa2 p-4 flex justify-center text-white font-light ">
+                <ul className="text-center flex flex-col gap-y-2">
+                    <li className="hover:text-gray-200 hover:cursor-pointer">Home</li>
+                    <li className="hover:text-gray-200 hover:cursor-pointer">Menu</li>
+                    <li className="hover:text-gray-200 hover:cursor-pointer">Saiba Mais</li>
                 </ul>
             </div>
         </div>
