9fe0ff93e9ee5823edc467c5ad98f14e4cf229b8
Maciel <macielsemr@gmail.com>
2024-06-25 13:56:51 -0300

    feat: implementando função do botão menu

--- src/componentes/header/header.tsx
+++ src/componentes/header/header.tsx
@@ -1,7 +1,13 @@
-import Image from "next/image"
+"use client";
+
+import Image from "next/image";
 import { IoIosMenu } from "react-icons/io";
+import { useState } from "react";
 
 export default function Header(){
+
+    const [estadoMenu, setEstadoMenu] = useState(true);
+
     return(
         <div className="mb-4">
             <div className="bg-rosa1">
@@ -16,12 +22,15 @@ export default function Header(){
                 </div>
                 <div className="bg-rosa2 flex justify-center py-2 shadow-lg">
                     <IoIosMenu 
+                        onClick={() => setEstadoMenu(!estadoMenu)}
                         className="text-slate-100 text-2xl hover:cursor-pointer"
                     />
                 </div>
                 
             </div>
-            <div className="bg-rosa2 p-4 flex justify-center text-white font-light ">
+            <div className={`
+                bg-rosa2 p-4 flex justify-center text-white font-light ${estadoMenu?'hidden':'visible'}
+                `}>
                 <ul className="text-center flex flex-col gap-y-2">
                     <li className="hover:text-gray-200 hover:cursor-pointer">Home</li>
                     <li className="hover:text-gray-200 hover:cursor-pointer">Menu</li>
